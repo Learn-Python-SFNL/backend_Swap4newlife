8f553aa2eee40e1c3477ff64307d59fddba1bcdb
Игорь Капинкин <i.kapinkin@mail.ru>
2023-03-06 13:21:14 +0300

    chose: Сделал простое flask приложение

--- backend.py
+++ backend.py
@@ -1,1 +1,11 @@
-print('backend проекта')
+from flask import Flask
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello World'
+
+if __name__=='__main__':
+    app.run()
+#http://127.0.0.1:5000
