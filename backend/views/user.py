b5423c07a049ce438ba51681cdf43a006d8f09eb
Игорь Капинкин <i.kapinkin@mail.ru>
2023-03-27 18:54:26 +0300

    chose: Сделал ручку для добавления пользователей,

---
+++ backend/views/user.py
@@ -0,0 +1,22 @@
+from http import HTTPStatus
+
+from flask import Blueprint, abort, jsonify, request
+
+from backend import schemas
+from backend.storages.user import UserStorage
+
+view = Blueprint('users', __name__)
+
+pgstorage = UserStorage()
+
+
+@view.post('/')
+def add_user():
+    payload = request.json
+    if not payload:
+        abort(HTTPStatus.BAD_REQUEST)
+
+    new_user = schemas.User(**payload)
+    user = pgstorage.add(new_user.tgid, new_user.username)
+
+    return jsonify(schemas.User.from_orm(user).dict())
